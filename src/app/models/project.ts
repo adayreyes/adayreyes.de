c5b58009e4c715fe05f6dbea8082beef4d6c7b19
adayreyes <adayreyes4a@gmail.com>
2022-05-16 20:23:51 +0200

    projects added

---
+++ src/app/models/project.ts
@@ -0,0 +1,13 @@
+export class Project {
+
+    public name;
+    public img;
+    public description;
+    public link;
+    constructor(name,description,img,link) {
+        this.name = name;
+        this.description = description;
+        this.img = img;
+        this.link = link
+    }
+}
--- src/app/work/work.component.ts
+++ src/app/work/work.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Project } from "../models/project";
 @Component({
   selector: 'app-work',
   templateUrl: './work.component.html',
@@ -12,4 +12,12 @@ export class WorkComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  projects = {
+    sharkie: new Project("Sharkie","Game based OPP with Javascript","sharkiepc.jpg","https://aday-reyes.developerakademie.net/Sharkie-game/index.html"),
+    join: new Project("Join","Game based OPP with Javascript","join.jpg","https://aday-reyes.developerakademie.net/Join/index.html"),
+    pokedex: new Project("Pokedex","Game based OPP with Javascript","pokedex.jpg","https://aday-reyes.developerakademie.net/Pokedex/index.html"),
+    ringoffire: new Project("Ring of fire","Game based OPP with Javascript","ringoffire.jpg","https://aday-reyes.developerakademie.net/ringoffire/"),
+  }
+  
+  
 }
