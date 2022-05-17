b73c689fb103bf341f742e818165cabd48ed991a
adayreyes <adayreyes4a@gmail.com>
2022-05-17 18:15:22 +0200

    projects filter

--- src/app/models/project.ts
+++ src/app/models/project.ts
@@ -4,10 +4,14 @@ export class Project {
     public img;
     public description;
     public link;
-    constructor(name,description,img,link) {
+    public css_class;
+    public language;
+    constructor(name,description,img,link,css_class,language) {
         this.name = name;
         this.description = description;
         this.img = img;
-        this.link = link
+        this.link = link;
+        this.css_class = css_class;
+        this.language = language
     }
 }
--- src/app/work/work.component.ts
+++ src/app/work/work.component.ts
@@ -7,17 +7,48 @@ import { Project } from "../models/project";
 })
 export class WorkComponent implements OnInit {
 
+  angular = true;
+  javascript = true;
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  projects = {
-    sharkie: new Project("Sharkie","Game based OPP with Javascript","sharkiepc.jpg","https://aday-reyes.developerakademie.net/Sharkie-game/index.html"),
-    join: new Project("Join","Game based OPP with Javascript","join.jpg","https://aday-reyes.developerakademie.net/Join/index.html"),
-    pokedex: new Project("Pokedex","Game based OPP with Javascript","pokedex.jpg","https://aday-reyes.developerakademie.net/Pokedex/index.html"),
-    ringoffire: new Project("Ring of fire","Game based OPP with Javascript","ringoffire.jpg","https://aday-reyes.developerakademie.net/ringoffire/"),
-  }
+  projects = [
+    new Project("Sharkie","Game based OPP with Javascript","sharkiepc.jpg","https://aday-reyes.developerakademie.net/Sharkie-game/index.html","sharkie","javascript"),
+    new Project("Join","Game based OPP with Javascript","join.jpg","https://aday-reyes.developerakademie.net/Join/index.html","join","javascript"),
+    new Project("Pokedex","Game based OPP with Javascript","pokedex.jpg","https://aday-reyes.developerakademie.net/Pokedex/index.html","pokedex","javascript"),
+    new Project("Ring of fire","Game based OPP with Javascript","ringoffire.jpg","https://aday-reyes.developerakademie.net/ringoffire/","ringoffire","angular"),
+  ]
   
+
+  showAllProjects(){
+    let projects = document.getElementsByClassName("project");
+    for (let i = 0; i < projects.length; i++) {
+      const project = projects[i];
+      project.classList.remove("none")
+    }
+  }
+
+  showAngularProjects(){
+    this.showAllProjects();
+    let javascript_projects = document.getElementsByClassName("javascript");
+    for (let i = 0; i < javascript_projects.length; i++) {
+      let project = javascript_projects[i];
+      project.classList.add("none");
+    }
+  }
+
+  showJavascriptProjects(){
+    this.showAllProjects();
+    let angular_projects = document.getElementsByClassName("angular");
+    for (let i = 0; i < angular_projects.length; i++) {
+      let project = angular_projects[i];
+      project.classList.add("none");
+    }
+  }
+
+  
+
   
 }
