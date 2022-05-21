eb97051566bd0c2e7d72ee17c8bf041c49c17095
Aday Reyes <adayreyes4a@gmail.com>
2022-05-21 18:57:44 +0200

    scroll animations added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Portfolio';
+
+  ngOnInit(): void {
+    AOS.init({
+            offset:"200",
+            delay:"50",
+            duration:"500",
+            easing:"ease-in-out",
+            mirror:"true",
+            once:"false",
+            anchorPlacement:"top"
+  })
+  }
+  
+
 }
